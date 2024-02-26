90860d3e37ace648117cd1beddd268ed723fdc31
Vinare <scrat-me@mail.ru>
2024-02-26 21:41:44 +0300

    header desktop finished

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const languageSelect = () => {
+  const select = document.querySelector(".header__navbar_select");
+  const listItems = select.querySelectorAll("li");
+
+  select.addEventListener("click", () => {
+    select.classList.toggle("open");
+  });
+
+  listItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      const language = item.dataset.language;
+
+      listItems.forEach((i) => {
+        i.classList.remove("active");
+
+        if (i.dataset.language === language) {
+          i.classList.add("active");
+        }
+      });
+    });
+  });
+};
+
+languageSelect();
