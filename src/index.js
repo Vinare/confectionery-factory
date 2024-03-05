d97728f9d6bf5db27071c169318177adeab309d9
Velmoren <velmoren@yahoo.com>
2024-03-06 00:02:03 +0300

    add about page

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import { languageSelect } from "./modules/language-select";
 import { searchInput } from "./modules/search-input";
 import { heroSlider } from "./modules/hero-slider";
 import { productsSlider } from "./modules/products-slider";
+import { ourTime } from "./modules/ourTime";
 
 languageSelect();
 searchInput();
 heroSlider();
 productsSlider(".products-slider");
 productsSlider(".new-products-slider");
+ourTime()
 
--- src/modules/hero-slider.js
+++ src/modules/hero-slider.js
@@ -3,85 +3,88 @@ import Splide from "@splidejs/splide";
 export const heroSlider = (delay = 4000) => {
   const heroWrapper = document.querySelector(".hero-wrapper");
   const tabWrapper = document.querySelector('.hero__slider_tabs')
-  const images = heroWrapper.querySelectorAll(".hero__image");
-  let tabs = heroWrapper.querySelectorAll(".hero-tab");
 
-  let activeIndex = 0;
-  let interval;
-  let tabSlider;
-
-  tabSlider = new Splide(".splide", {
-    arrows: false,
-    pagination: false,
-    // clones: 0,
-    type: "loop",
-    focus: "center",
-    autoWidth: true,
-    mediaQuery: "min",
-    gap: 12,
-    breakpoints: {
-      992: { destroy: true },
-    },
-  }).mount();
-
-  const changeSlide = (idx) => {
-    tabs = heroWrapper.querySelectorAll(".hero-tab");
-
-    images.forEach((image, imageIndex) => {
-      image.classList.remove("active");
-
-      if (imageIndex === idx) {
-        image.classList.add("active");
+  if (heroWrapper) {
+    const images = heroWrapper.querySelectorAll(".hero__image");
+    let tabs = heroWrapper.querySelectorAll(".hero-tab");
+  
+    let activeIndex = 0;
+    let interval;
+    let tabSlider;
+  
+    tabSlider = new Splide(".splide", {
+      arrows: false,
+      pagination: false,
+      // clones: 0,
+      type: "loop",
+      focus: "center",
+      autoWidth: true,
+      mediaQuery: "min",
+      gap: 12,
+      breakpoints: {
+        992: { destroy: true },
+      },
+    }).mount();
+  
+    const changeSlide = (idx) => {
+      tabs = heroWrapper.querySelectorAll(".hero-tab");
+  
+      images.forEach((image, imageIndex) => {
+        image.classList.remove("active");
+  
+        if (imageIndex === idx) {
+          image.classList.add("active");
+        }
+      });
+  
+      tabs.forEach((tab, tabIndex) => {
+        tab.classList.remove("active");
+  
+        if (Number(tab.dataset.slideIdx) === idx) {
+          tab.classList.add("active");
+        }
+      });
+  
+      if (tabSlider && !tabSlider.options.destroy) {
+          tabSlider.go(idx)
       }
-    });
-
-    tabs.forEach((tab, tabIndex) => {
-      tab.classList.remove("active");
-
-      if (Number(tab.dataset.slideIdx) === idx) {
-        tab.classList.add("active");
+    };
+  
+    const autoplay = () => {
+      interval = setInterval(() => {
+        console.log(activeIndex);
+        if (activeIndex === 4) {
+          activeIndex = 0;
+        } else {
+          activeIndex++;
+        }
+  
+        changeSlide(activeIndex);
+      }, delay);
+    };
+  
+    const startAutoplay = () => {
+      autoplay();
+    };
+  
+    const stopAutoplay = () => {
+      clearInterval(interval);
+    };
+  
+    tabWrapper.addEventListener('click', (e) => {
+      if (e.target.closest('.hero-tab')) {
+        const tab = e.target.closest('.hero-tab')
+        const tabIndex = Number(tab.dataset.slideIdx)
+  
+        activeIndex = tabIndex;
+  
+        changeSlide(tabIndex);
+        stopAutoplay()
+        startAutoplay()
       }
-    });
-
-    if (tabSlider && !tabSlider.options.destroy) {
-        tabSlider.go(idx)
-    }
-  };
-
-  const autoplay = () => {
-    interval = setInterval(() => {
-      console.log(activeIndex);
-      if (activeIndex === 4) {
-        activeIndex = 0;
-      } else {
-        activeIndex++;
-      }
-
-      changeSlide(activeIndex);
-    }, delay);
-  };
-
-  const startAutoplay = () => {
+    })
+  
+    changeSlide(activeIndex);
     autoplay();
-  };
-
-  const stopAutoplay = () => {
-    clearInterval(interval);
-  };
-
-  tabWrapper.addEventListener('click', (e) => {
-    if (e.target.closest('.hero-tab')) {
-      const tab = e.target.closest('.hero-tab')
-      const tabIndex = Number(tab.dataset.slideIdx)
-
-      activeIndex = tabIndex;
-
-      changeSlide(tabIndex);
-      stopAutoplay()
-      startAutoplay()
-    }
-  })
-
-  changeSlide(activeIndex);
-  autoplay();
+  }
 };
