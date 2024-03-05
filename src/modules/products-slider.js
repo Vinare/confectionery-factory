5542ca53a1a84ac6f3ee6440ec5dc836ce4313a4
Vinare <scrat-me@mail.ru>
2024-03-05 23:53:54 +0300

    responsive in progress

--- src/modules/products-slider.js
+++ src/modules/products-slider.js
@@ -3,32 +3,39 @@ import Splide from "@splidejs/splide";
 export const productsSlider = (sliderSelector) => {
   const initSlider = () => {
     new Splide(sliderSelector, {
-        arrows: false,
-        pagination: false,
-        type: "loop",
-        focus: "center",
-        fixedWidth: 140,
-        mediaQuery: "min",
-        gap: 12,
-        breakpoints: {
+      arrows: false,
+      pagination: false,
+      type: "loop",
+      focus: "center",
+      fixedWidth: 140,
+      fixedHeight: 260,
+      mediaQuery: "min",
+      gap: 12,
+      breakpoints: {
+        532: {
+          fixedWidth: null,
+          fixedHeight: null,
+          perPage: 3,
+          focus: 1,
+        },
         768: {
-            fixedWidth: null,
-            perPage: 2,
-            gap: 24,
-            focus: 1,
+          fixedWidth: null,
+          perPage: 2,
+          gap: 24,
+          focus: 1,
         },
         992: {
-            perPage: 3,
-            gap: 24,
-            focus: 1,
+          perPage: 3,
+          gap: 24,
+          focus: 1,
         },
         1400: {
-            perPage: 4,
-            gap: 24,
-            focus: 1,
-        }
+          perPage: 4,
+          gap: 24,
+          focus: 1,
         },
-      }).mount();
+      },
+    }).mount();
   };
 
   initSlider();
