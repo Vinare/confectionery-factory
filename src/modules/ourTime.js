6cc6616f84f78f31937f6650293d428bef709968
Velmoren <velmoren@yahoo.com>
2024-03-06 00:02:03 +0300

    add about page

---
+++ src/modules/ourTime.js
@@ -0,0 +1,21 @@
+import Splide from "@splidejs/splide";
+
+export const ourTime = () => {
+  const initSlider = () => {
+    new Splide(".our-time__slider", {
+      arrows: false,
+      pagination: false,
+      clones: 0,
+      type: "loop",
+      focus: "center",
+      gap: 24,
+      breakpoints: {
+        767: {
+          destroy: true
+        },
+      },
+    }).mount();
+  };
+
+  initSlider();
+};
