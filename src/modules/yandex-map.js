5bd264d7fadc497aee5bf36cdaec8d9add5cce65
Velmoren <velmoren@yahoo.com>
2024-03-11 22:58:38 +0300

    fix maps

--- src/modules/yandex-map.js
+++ src/modules/yandex-map.js
@@ -1,42 +1,41 @@
 export const yandexMap = () => {
-  window.map = null;
-
   async function initMap() {
+    let ymaps3 = window.ymaps3;
     let map;
 
     await ymaps3.ready;
 
-    const {
-      YMap,
-      YMapDefaultSchemeLayer,
-      YMapDefaultFeaturesLayer,
-      YMapMarker,
-    } = ymaps3;
+    const { YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer, YMapMarker } = ymaps3;
 
     map = new YMap(
       document.getElementById("map"),
       {
         location: {
-          center: [46.00891089556499, 51.57077959379591],
+          center: [
+            46.00891089556499,
+            51.57077959379591
+        ],
           zoom: 17,
         },
+        behaviors: ['drag', 'pinchZoom', 'mouseTilt']
       },
-      [new YMapDefaultSchemeLayer({}), new YMapDefaultFeaturesLayer({})]
+      [
+        new YMapDefaultSchemeLayer({}),
+        new YMapDefaultFeaturesLayer({}),
+      ]
     );
 
-    const markerElement = document.createElement("img");
-    markerElement.className = "contacts-map--icon-marker";
-    markerElement.style.width = "89px";
-    markerElement.style.height = "117px";
-    markerElement.style.position = "relative";
-    markerElement.style.transform = "translate(-50%, -50%)";
-    markerElement.src = "/images/icons/marker.png";
-    map.addChild(
-      new YMapMarker(
-        { coordinates: [46.00829728258972, 51.57158353822383] },
-        markerElement
-      )
-    );
+    const markerElement = document.createElement('img');
+    markerElement.className = 'contacts-map--icon-marker';
+    markerElement.style.width = '89px'
+    markerElement.style.height = '117px'
+    markerElement.style.position = 'relative'
+    markerElement.style.transform = 'translate(-50%, -50%)'
+    markerElement.src = '/images/icons/marker.png';
+    map.addChild(new YMapMarker({coordinates: [
+        46.00829728258972,
+        51.57158353822383
+    ]}, markerElement));
   }
 
   initMap();
