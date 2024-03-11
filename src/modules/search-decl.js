a67bd64acba2c54c99961f9fe385c65f2e82c071
Velmoren <velmoren@yahoo.com>
2024-03-11 17:36:31 +0300

    fix decl search function

--- src/modules/search-decl.js
+++ src/modules/search-decl.js
@@ -8,7 +8,7 @@ export const searchDecl = () => {
         const description = searchSection.querySelector('.search-description');
         const searchCount = Number(searchCards.length);
 
-        const firstStr = declOfNum(searchCount, ['найдена', 'найдено', 'найдены']);
+        const firstStr = declOfNum(searchCount, ['найден', 'найдено', 'найдены']);
         const secondStr = declOfNum(searchCount, ['результат', 'результата', 'результатов']);
         
         description.textContent = `${firstStr} ${searchCount} ${secondStr}`
