0b9cfa6d969ebcc75d601673b91c884cce1a2a93
Velmoren <velmoren@yahoo.com>
2024-03-16 01:51:39 +0300

    remove favicons

--- src/modules/mobile-menu.js
+++ src/modules/mobile-menu.js
@@ -1,5 +1,3 @@
-import { disableBodyScroll, clearAllBodyScrollLocks } from "body-scroll-lock";
-
 export const mobileMenu = () => {
   const btn = document.querySelector(".header__navbar_button");
   const mobileMenu = document.querySelector(".header-menu .-mobile");
@@ -10,13 +8,11 @@ export const mobileMenu = () => {
     btn.addEventListener("click", () => {
       mobileMenu.style.display = "block";
       setTimeout(() => mobileMenu.classList.add("-active"));
-      disableBodyScroll(document.body);
     });
 
     close.addEventListener("click", () => {
       mobileMenu.classList.remove("-active");
       setTimeout(() => (mobileMenu.style.display = null), 200);
-      clearAllBodyScrollLocks(document.body);
     });
   }
   
